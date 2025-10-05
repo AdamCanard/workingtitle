92b23896eb54ca3e69744cceadb7de14c123c250
adamcanard <azcunard@gmail.com>
2025-10-05 10:58:09 -0600

    feat: formrenderer

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,38 @@ export default function Home() {
         "flex w-full h-full justify-center items-center px-4 bg-background"
       }
     >
-      <div
-        className={
-          "border-2 border-black rounded-md  w-full h-full max-w-[480px] max-h-2/3 bg-foreground"
-        }
-      ></div>
+      <Card />
     </main>
   );
+}
+interface IFormInput {
+  label: string;
+  name: string;
+  regex: string;
+}
+function Card() {
+  const formInputs: IFormInput[] = [
+    { label: "First Name", name: "firstName", regex: "[A-Za-z]" },
+    { label: "Last Name", name: "lastName", regex: "[A-Za-z]" },
+    {
+      label: "Email",
+      name: "email",
+      regex: "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$",
+    },
+  ];
+
+  return (
+    <div
+      className={
+        "border-2 border-black rounded-md w-full h-full max-w-[480px] max-h-2/3 bg-foreground"
+      }
+    >
+      <FormRenderer inputs={formInputs} />
+    </div>
+  );
+}
+
+function FormRenderer(props: { inputs: IFormInput[] }) {
+  console.log(props.inputs);
+  return <></>;
 }
